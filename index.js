6c002ee776e9bca642c6267d05fab39d044aafc9
Vivian <vivian.bonilla@outlook.com>
2025-01-31 10:08:35 -0700

    Reworked

---
+++ index.js
@@ -0,0 +1,9 @@
+function one() {
+    window.open("https://www.youtube.com/watch?v=0RVeo79yfw0", "popupWindow", 'width=600,height=400,top=100,left=100,noopener,noreferrer');
+}
+function two() {
+    window.open("https://youtu.be/YP3jRz9kJXs?feature=shared", "popupWindow", 'width=600,height=400,top=100,left=100,noopener,noreferrer');
+}
+function three() {
+    window.open("https://youtu.be/T1EJZsaqevU?feature=shared", "popupWindow", 'width=600,height=400,top=100,left=100,noopener,noreferrer');
+}
